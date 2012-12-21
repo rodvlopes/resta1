b6c911761c6778cae67194e912212686eda6ac6b
Rodrigo Lopes <rodvlopes@gmail.com>
2012-12-20 22:08:29 -0500

    conclusao teorica do algoritmo genetico. falta integrar

--- javascript/src/Utils.js
+++ javascript/src/Utils.js
@@ -8,6 +8,16 @@ Array.prototype.contains = function(obj) {
   return false;
 }
 
+String.prototype.contains = function(obj) {
+  var i = this.length;
+  while (i--) {
+    if (this[i] === obj) {
+      return true;
+    }
+  }
+  return false;
+}
+
 Notification = {
 	
 	generic : function(msg, header, type) {
